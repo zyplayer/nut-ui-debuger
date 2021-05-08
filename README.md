ba57dc98d7b1996eab917e45548ca2a6d3a522c3
暮光：城中城 <806783409@qq.com>
2021-05-08 12:38:20 +0800

    BUG：navbar里面添加的tabs展示不出来

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# nut-ui-debuger
-nut-ui的bug复现最小单元
+vue3：https://v3.cn.vuejs.org/
+
+vue router：https://next.router.vuejs.org/zh/guide/
+
+nutui：https://nutui.jd.com/3x/#/intro
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    BUG：navbar里面添加的tabs展示不出来
+    <nut-navbar @on-click-back="" @on-click-title="" @on-click-clear="" @on-click-send="" desc="编辑" icon="horizontal">
+        <template v-slot:tabs>
+            <nut-tab>
+                <nut-tab-panel tab-title="商品"><p class="content">这里的tab展示不出来</p></nut-tab-panel>
+                <nut-tab-panel tab-title="店铺"><p class="content">这里的tab展示不出来</p></nut-tab-panel>
+            </nut-tab>
+        </template>
+    </nut-navbar>
+</template>
+
+<script setup>
+</script>
+
+<style>
+    html, body {
+        margin: 0;
+        padding: 0;
+    }
+
+    #app {
+    }
+</style>
